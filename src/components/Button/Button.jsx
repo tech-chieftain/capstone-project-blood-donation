fa3a41dc6a1991dc5584384fe09702da7c8655dc
saleem <salem1234saleem@gmail.com>
2020-10-23 17:08:37 +0300

    apply the requested changes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-function Button({ text, onclick, classFlag }) { 
+function Button({ text, onclick, classFlag }) {
   const classesObject = {
     // white button
     buttonWhite:
-      'inline-block text-primary-100 text-base md:text-lg  text-center md:font-medium bg-white  md:mt-8 mt-4 px-8 md:px-12 lg:px-16 py-1 lg:py-2 rounded shadow-md transition duration-500 ease-in-out transform hover:scale-95',
+      'inline-block text-primary-100 text-base md:text-lg  text-center md:font-medium bg-white  md:mt-8 mt-4 px-8 md:px-12 lg:px-16 py-1 lg:py-2  shadow-md transition duration-500 ease-in-out transform hover:scale-95',
     // red button
     buttonRed:
-      'inline-block text-white text-base md:text-lg  text-center md:font-medium bg-primary  md:mt-8 mt-4 px-8 md:px-12 lg:px-16 py-1 lg:py-2 rounded shadow-md transition duration-500 ease-in-out hover:bg-primary-100',
+      'inline-block text-white text-base md:text-lg  text-center md:font-medium bg-primary  md:mt-8 mt-4 px-8 md:px-12 lg:px-16 py-1 lg:py-2  shadow-md transition duration-500 ease-in-out transform hover:scale-95',
   };
   return (
     <button
@@ -24,7 +24,7 @@ function Button({ text, onclick, classFlag }) {
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
-  onclick: PropTypes.func,
+  onclick: PropTypes.func.isRequired,
   classFlag: PropTypes.bool,
 };
 export default Button;
