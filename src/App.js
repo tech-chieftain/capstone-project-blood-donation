42624efc789bb6effe79ffd9daf49ace60f86ef6
Osamah Gharad <osamah.gharred@gmail.com>
2020-10-23 23:39:20 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/main.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <p className="text-black">learn react</p>
+      <Footer />
     </div>
   );
 }
