f8a056a716c9215dbd9d25d339eb8e5a098cc6ac
Emad  Alhattami <emad774373827@gmail.com>
2020-10-24 06:43:39 +0300

    this for test components card close#13

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CardMain from './Components/Cards/CardsMain';
+import CardUpoutUs from './Components/Cards/CardsAboutUs';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <CardMain />
+    <CardUpoutUs />
   </React.StrictMode>,
   document.getElementById('root')
 );
