3c7c51a5dc79c7132180839ce158a5c4d783acdc
Shihab Mekhlafi <shihabalmekhlafy@gmail.com>
2020-10-24 17:41:35 +0300

    feat: make changes as reviewers ask

--- src/components/RegistrationCard/RegistrationCard.jsx
+++ src/components/RegistrationCard/RegistrationCard.jsx
@@ -3,13 +3,13 @@ import { PropTypes } from 'prop-types';
 
 function RegistrationCard({ icon, title, description }) {
     return (
-        <div class="flex bg-gray-200 rounded p-6 m-6">
+        <div className="flex bg-gray-200 rounded p-6 m-6">
             {/*  icon place */}
             {/* icon will be on svg tag */}
-            <div class="flex-none h-16 w-16  md:h-24 md:w-24 rounded-full mx-auto mr-2 sm:mr-4 border-4 sm:border-6   border-gray-500">
-                {icon}
+            <div className="flex-none h-16 w-16  md:h-24 md:w-24 rounded-full mx-auto mr-2 sm:mr-4 border-4 sm:border-6   border-gray-500">
+                <img src={icon} alt="icon" />
             </div>
-            <div className="  flex-1 my-6 mr-4 sm:mr-4">
+            <div className="flex-1 my-6 mr-4 sm:mr-4">
                 {/* title place */}
                 <span className="text-primary-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">{title} - </span>
                 {/* description place */}
