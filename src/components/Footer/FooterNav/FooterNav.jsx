aeed2f424125a45cf4a0dbf244766734d7dcd543
Osamah Gharad <osamah.gharred@gmail.com>
2020-10-26 17:00:33 +0300

    saving changes

--- src/components/Footer/FooterNav/FooterNav.jsx
+++ src/components/Footer/FooterNav/FooterNav.jsx
@@ -1,23 +1,19 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from '../../../containers/Home';
-import About from '../../../containers/About';
-import Instruction from '../../../containers/Instruction';
-import Process from '../../../containers/Process';
-import Search from '../../../containers/Search';
-import Donate from '../../../containers/Donate';
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+// import Home from '../../../containers/Home';
+// import About from '../../../containers/About';
+// import Instruction from '../../../containers/Instruction';
+// import Process from '../../../containers/Process';
+// import Search from '../../../containers/Search';
+// import Donate from '../../../containers/Donate';
 
-function FooterNav(){
-
-    const NavDesign ='px-2 py-2 lg:px-6  mt-4 md:mt-0 sm:mr-0 md:mr-2 lg:mr-4 text-sm  text-white tracking-wide inline-block border-b-2 border-transparent hover:border-white'
-    return (
-<Router>
-<div classname="flex justify-center flex-wrap">
+function FooterNav() {
+  const NavDesign =
+    'px-2 py-2 lg:px-6  mt-4 md:mt-0 sm:mr-0 md:mr-2 lg:mr-4 text-sm  text-white tracking-wide inline-block border-b-2 border-transparent hover:border-white';
+  return (
+    <Router>
+      <div className="flex justify-center flex-wrap">
         <nav>
           <ul className=" text-white sm:flex justify-center flex-wrap ">
             <li className={NavDesign}>
@@ -40,31 +36,29 @@ function FooterNav(){
             </li>
           </ul>
         </nav>
-          
-        <Switch>
+
+        {/* <Switch>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/instruction">
-            <Instruction/>
+            <Instruction />
           </Route>
           <Route path="/process">
-            <Process/>
+            <Process />
           </Route>
           <Route path="/search">
-            <Search/>
+            <Search />
           </Route>
           <Route path="/donate">
-            <Donate/>
+            <Donate />
           </Route>
           <Route path="/">
             <Home />
           </Route>
-        </Switch>
-        </div>
-
-</Router>
-      
-    )
+        </Switch> */}
+      </div>
+    </Router>
+  );
 }
-export default FooterNav
+export default FooterNav;
