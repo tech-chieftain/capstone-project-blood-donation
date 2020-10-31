9f72a98a2580aaac5d581d730c5638d90c1fcef8
Emad  Alhattami <emad774373827@gmail.com>
2020-10-31 21:05:38 +0300

    update test

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,13 +3,13 @@ import { PropTypes } from 'prop-types';
 
 function Card({ cardImage, cardTitle, cardDesc, orientationText }) {
   const classesObjectStyles = {
-    cardTitle: `text-primary-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold my-3 text-${orientationText}`,
-    cardParagraph: `text-black text-xl sm:text-2xl md:text-3xl lg:text-4xl text-${orientationText}`,
+    cardTitle: `text-primary-100 text-lg sm:text-xl md:text-2xl font-bold my-3 text-${orientationText}`,
+    cardParagraph: `text-black text-sm sm:text-base md:text-lg lg:text-xl text-${orientationText}`,
   };
   return (
-    <div className="max-w-3xl my-5 ml-6 mr-6 bg-gray-200 rounded sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/3">
-      <div className="flex justify-center">
-        <img className="w-1/3 h-1/3" src={cardImage} alt="icon" />
+    <div className="rounded">
+      <div className="flex justify-center w-full h-40">
+        <img src={cardImage} alt="icon" />
       </div>
 
       <div className="px-6 py-0 pb-6 my-5">
