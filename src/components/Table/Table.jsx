00c098c395ab757666a1d16c6d7e5f1060cb0fea
OsamahGharad <46979505+OsamahGharad@users.noreply.github.com>
2020-11-04 15:46:38 +0300

    Add table translation

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,57 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function Table() {
-    const StylesClass = {
-        grayColor: 'w-full bg-gray-200',
-        whiteColor: 'we-full bg-red',
-        ceilClass: 'border px-4 py-2 lg:px-6 mt-4 md:mt-0  whitespace-no-wrap border-b bg-gray',
-        headerStyle:'border px-4 py-2 text-white leading-4 text-sm tracking-wider text-left'
-    }
-  
-        const donors = [
-            {
-                id: 1,
-                name: 'ali',
-                city: 'sanaa',
-                bloodType: 'A+',
-                phone: '772455696',
-                email: 'ali@gmail.com',
-            },
-            {
-                id: 2,
-                name: 'mohamed',
-                city: 'Aden',
-                bloodType: 'A+',
-                phone: '772455696',
-                email: 'mo@gmail.com',
-            }
-        ];
-   
-//   const renderDonors = donors.map((donor) => <DonorRow key={donor.id} {...donor} />);
-     const renderDonors = donors.map((donor) =>{
-         return (
-            <tr className={donor.id % 2 === 0 ? StylesClass.grayColor : StylesClass.whiteColor}>
-        <td className={StylesClass.ceilClass}>{donor.name}</td>
-        <td className={StylesClass.ceilClass}>{donor.city}</td>
-        <td className={StylesClass.ceilClass}>{donor.bloodType}</td>
-        <td className={StylesClass.ceilClass}>{donor.phone}</td>
-        <td className={StylesClass.ceilClass}>{donor.email}</td>
-      </tr> 
-         )
-     }
-     );
-    
+  const { t } = useTranslation();
+  const StylesClass = {
+    grayColor: 'w-full bg-gray-200',
+    whiteColor: 'we-full bg-red',
+    ceilClass: 'border px-4 py-2 text-center lg:px-6 mt-4 md:mt-0  whitespace-no-wrap border-b bg-gray',
+    headerStyle: 'border px-4 py-2 text-white text-center leading-4 text-sm tracking-wider text-left',
+  };
+
+  const donors = [
+    {
+      id: 1,
+      name: 'ali',
+      city: 'sanaa',
+      bloodType: 'A+',
+      phone: '772455696',
+      email: 'ali@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'mohamed',
+      city: 'Aden',
+      bloodType: 'A+',
+      phone: '772455696',
+      email: 'mo@gmail.com',
+    },
+  ];
+
+  //   const renderDonors = donors.map((donor) => <DonorRow key={donor.id} {...donor} />);
+  const renderDonors = donors.map((donor) => (
+    <tr className={donor.id % 2 === 0 ? StylesClass.grayColor : StylesClass.whiteColor}>
+      <td className={StylesClass.ceilClass}>{donor.name}</td>
+      <td className={StylesClass.ceilClass}>{donor.bloodType}</td>
+      <td className={StylesClass.ceilClass}>{donor.city}</td>
+      <td className={StylesClass.ceilClass}>{donor.phone}</td>
+      <td className={StylesClass.ceilClass}>{donor.email}</td>
+    </tr>
+  ));
+
   return (
-     
     <div>
       <table className="border table-auto  min-w-full">
         <thead>
           <tr className="bg-primary">
-            <th className={StylesClass.headerStyle}>Name</th>
-            <th className={StylesClass.headerStyle}>City</th>
-            <th className={StylesClass.headerStyle}>Blood Type</th>
-            <th className={StylesClass.headerStyle}>Phone</th>
-            <th className={StylesClass.headerStyle}>Email</th>
+            <th className={StylesClass.headerStyle}>{t('table.name')}</th>
+            <th className={StylesClass.headerStyle}>{t('table.bloodType')}</th>
+            <th className={StylesClass.headerStyle}>{t('table.city')}</th>
+            <th className={StylesClass.headerStyle}>{t('table.phone')}</th>
+            <th className={StylesClass.headerStyle}>{t('table.email')}</th>
           </tr>
         </thead>
         <tbody>{renderDonors}</tbody>
