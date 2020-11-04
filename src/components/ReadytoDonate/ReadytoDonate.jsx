a85b1f7c377b6037eaf5cbecb7776f62063977a5
OsamahGharad <46979505+OsamahGharad@users.noreply.github.com>
2020-11-04 19:05:20 +0300

    save

--- src/components/ReadytoDonate/ReadytoDonate.jsx
+++ src/components/ReadytoDonate/ReadytoDonate.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import Button from '../Button';
+import { useTranslation } from 'react-i18next';
 
 function ReadytoDonate({ header, text }) {
   const history = useHistory();
+  const { t } = useTranslation();
   return (
     <div>
-      <div className="text-black text-lg sm:text-xl md:text-2xl font-semibold">{header}</div> 
-      <div className="text-black w-6/12 text-sm sm:text-base md:text-lg tracking-wide leading-wide">{text}</div>
+      <div className="text-black text-lg sm:text-xl md:text-2xl font-semibold">
+        {t('ready_donate.header')}
+      </div>
+      <div className="text-black w-6/12 text-sm sm:text-base md:text-lg tracking-wide leading-wide">
+        {t('ready_donate.text')}
+      </div>
       <div>
         <Button text="Donate" onclick={() => history.push('./Donate')} />
       </div>
@@ -22,7 +28,7 @@ ReadytoDonate.propTypes = {
 };
 // Specifies the default values for props:
 ReadytoDonate.defaultProps = {
-  header: 'Ready to Donate?',
-  text: 'It does not make much time and every donation has the power to save up to three lives',
+  header: 'ready_donate.header',
+  text: 'ready_donate.text',
 };
 export default ReadytoDonate;
