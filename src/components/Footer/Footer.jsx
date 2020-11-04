9c892afbb4fc51644f23266ff4971bd2671359c3
OsamahGharad <46979505+OsamahGharad@users.noreply.github.com>
2020-11-04 23:35:58 +0300

    add translation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import SocialIcons from './SocialIcons/SocialIcons';
 import NavLinks from '../Navlinks';
 import Logo from '../images/Logo';
+import { useTranslation } from 'react-i18next';
 
 function Footer() {
+  const { t } = useTranslation();
   return (
     <div>
       <div className="bg-primary flex flex-col justify-center items-center p-4 sm:p-6 md:p-8 ">
@@ -12,10 +14,7 @@ function Footer() {
         </div>
         <div className="w-8/12 lg:w-6/12">
           {/* text-black text-xl sm:text-1xl md:text-2xl lg:text-2xl */}
-          <p className="text-white text-center mb-2">
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-            velit
-          </p>
+          <p className="text-white text-center mb-2">{t('footer_text')}</p>
         </div>
         <div>
           <NavLinks classname="flex justify-center flex-wrap" classFlag />
