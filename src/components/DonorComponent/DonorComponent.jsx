ed64813e4c4241b13d685f0736857a7fe2b23f21
Emad  Alhattami <emad774373827@gmail.com>
2020-11-08 04:38:33 +0300

    Update DonorCompnent

--- src/components/DonorComponent/DonorComponent.jsx
+++ src/components/DonorComponent/DonorComponent.jsx
@@ -1,44 +1,29 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import name from '../images/name.png';
-import bloodType from '../images/blood_type.png';
-import phone from '../images/phone.png';
-import addresIco from '../images/location.png';
+import name from '../images/donorcomponent/name.svg';
+import blood from '../images/donorcomponent/blood_type.svg';
+import phone from '../images/donorcomponent/phone.svg';
+import addresIco from '../images/donorcomponent/location.svg';
+import emailIco from '../images/donorcomponent/email.svg';
+import DonorCard from '../DonorCard/DonorCard';
 
-function DonorComponent({ donorName, blodType, phoneNumber, address }) {
-  const classesObjectStyles = {
-    cardDonor: 'max-w-sm px-1 py-1 m-10 mx-6 bg-gray-200 sm:w-full',
-    cardIcon: 'w-6 h-6 px-1 my-3 mr-2',
-    cardTitle: 'pt-2 text-2xl font-bold sm:text-2xl md:text-2xl lg:text-2xl',
-  };
-
+function DonorComponent({ donorName, bloodType, phoneNumber, address, email }) {
   return (
-    <div className={classesObjectStyles.cardDonor}>
-      <div className="flex">
-        <img className={classesObjectStyles.cardIcon} src={name} alt="icon" />
-        <span className={classesObjectStyles.cardTitle}> {donorName} </span>
-      </div>
-
-      <div className="flex">
-        <img className={classesObjectStyles.cardIcon} src={bloodType} alt="icon" />
-        <span className={classesObjectStyles.cardTitle}>{blodType} </span>
-      </div>
-      <div className="flex">
-        <img className={classesObjectStyles.cardIcon} src={phone} alt="icon" />
-        <span className={classesObjectStyles.cardTitle}> {phoneNumber}</span>
-      </div>
-      <div className="flex">
-        <img className={classesObjectStyles.cardIcon} src={addresIco} alt="icon" />
-        <span className={classesObjectStyles.cardTitle}> {address}</span>
-      </div>
+    <div>
+      <DonorCard title={donorName} cardIcon={name} />
+      <DonorCard title={bloodType} cardIcon={blood} />
+      <DonorCard title={phoneNumber} cardIcon={phone} />
+      <DonorCard title={address} cardIcon={addresIco} />
+      <DonorCard title={email} cardIcon={emailIco} />
     </div>
   );
 }
 
 DonorComponent.propTypes = {
   donorName: PropTypes.element.isRequired,
-  blodType: PropTypes.string.isRequired,
+  bloodType: PropTypes.string.isRequired,
   phoneNumber: PropTypes.string.isRequired,
   address: PropTypes.string.isRequired,
+  email: PropTypes.element.isRequired,
 };
 export default DonorComponent;
