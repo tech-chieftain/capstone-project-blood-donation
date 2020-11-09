03ea4827c9416194f607d342117355aad2a5df79
Emad  Alhattami <emad774373827@gmail.com>
2020-11-10 00:00:51 +0300

    update change

--- src/components/DonorCard/DonorCard.jsx
+++ src/components/DonorCard/DonorCard.jsx
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types';
 
 function DonorCard({ title, cardIcon }) {
   const classesObjectStyles = {
-    cardDonor: 'max-w-sm px-1  bg-gray-200 sm:w-full',
+    cardDonor: 'px-1  bg-gray-200 sm:w-full',
     cardIcon: 'w-6 h-6 px-1 my-3 mr-2',
     cardTitle: 'pt-3 text-lg sm:text-2xl md:text-2xl font-bold',
   };
