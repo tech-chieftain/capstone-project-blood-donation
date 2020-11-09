6184d4462d10c48503a68c3e0d1c1e67b8c65d4f
OsamahGharad <46979505+OsamahGharad@users.noreply.github.com>
2020-11-10 01:28:51 +0300

    save changes

--- src/containers/Search/Search.jsx
+++ src/containers/Search/Search.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 import Table from '../../components/Table';
 
 function Search() {
-  // const donors = [
-  //   {
-  //     id: 1,
-  //     name: 'ali',
-  //     city: 'sanaa',
-  //     bloodType: 'A+',
-  //     phone: '772455696',
-  //     email: 'ali@gmail.com',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'mohamed',
-  //     city: 'Aden',
-  //     bloodType: 'A+',
-  //     phone: '772455696',
-  //     email: 'mo@gmail.com',
-  //   },
-  // ];
+  const donors = [
+    {
+      id: 1,
+      name: 'ali',
+      city: 'sanaa',
+      bloodType: 'A+',
+      phone: '772455696',
+      email: 'ali@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'mohamed',
+      city: 'Aden',
+      bloodType: 'A+',
+      phone: '772455696',
+      email: 'mo@gmail.com',
+    },
+  ];
 
-  return <div>Search</div>;
+  return (
+    <div>
+      Search
+      <Table donors={donors} />
+    </div>
+  );
 }
 
 export default Search;
