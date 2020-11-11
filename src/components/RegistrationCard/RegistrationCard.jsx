7a93e008f8a073e2236d1e9b45d4ab9bb631ec4c
Emad  Alhattami <emad774373827@gmail.com>
2020-11-11 23:59:28 +0300

    update registration card & test

--- src/components/RegistrationCard/RegistrationCard.jsx
+++ src/components/RegistrationCard/RegistrationCard.jsx
@@ -3,22 +3,23 @@ import { PropTypes } from 'prop-types';
 
 function RegistrationCard({ icon, title, description }) {
   return (
-    <div className="flex bg-gray-200 rounded p-6 m-6">
-      {/*  icon place */}
-      {/* icon will be on svg tag */}
-      <div className="flex-none h-16 w-16  md:h-24 md:w-24 rounded-full mx-auto mr-2 sm:mr-4 border-4 sm:border-6 border-gray-500">
-        <img src={icon} alt="icon" />
-      </div>
+    <div className="flex flex-col p-6 m-6 bg-gray-200 rounded md:flex-row">
+      {' '}
+      {/* icon place */} {/* icon will be on svg tag */}{' '}
+      <div className="flex-none w-16 h-16 mx-auto border-4 border-gray-500 rounded-full md:h-24 md:w-24 sm:m-0 md:mr-4 sm:border-6 ">
+        {' '}
+        <img src={icon} alt="icon" />{' '}
+      </div>{' '}
       <div className="flex-1 my-6 mr-4 sm:mr-4">
-        {/* title place */}
-        <span className="text-primary-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+        {' '}
+        {/* title place */}{' '}
+        <span className="text-base font-bold text-primary-100 sm:text-sm md:text-lg">
+          {' '}
           {title} -{' '}
-        </span>
-        {/* description place */}
-        <span className="text-black text-xl sm:text-2xl md:text-3xl lg:text-4xl">
-          {description}
-        </span>
-      </div>
+        </span>{' '}
+        {/* description place */}{' '}
+        <span className="text-base text-black sm:text-sm md:text-lg">{description}</span>{' '}
+      </div>{' '}
     </div>
   );
 }
