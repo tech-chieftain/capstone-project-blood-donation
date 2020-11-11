621346c0a2028a823f5a8e6cffec28aa227f154c
Saleem Saleh Mogahed Alfaig <63640656+SaleemSaleh@users.noreply.github.com>
2020-11-12 01:38:37 +0300

    Close #73 arrange the layout of search page

--- src/components/DonorCard/DonorCard.jsx
+++ src/components/DonorCard/DonorCard.jsx
@@ -3,14 +3,14 @@ import { PropTypes } from 'prop-types';
 
 function DonorCard({ title, cardIcon }) {
   const classesObjectStyles = {
-    cardDonor: 'px-1  bg-gray-200 sm:w-full',
-    cardIcon: 'w-6 h-6 px-1 my-3 mr-2',
-    cardTitle: 'pt-3 text-lg sm:text-2xl md:text-2xl font-bold',
+    cardDonor: 'px-2 sm:px-4 py-2 bg-gray-200 ',
+    cardIcon: 'w-6 h-6 mx-2 my-1 object-cover ',
+    cardTitle: ' text-sm sm:text-base ',
   };
 
   return (
     <div className={classesObjectStyles.cardDonor}>
-      <div className="flex">
+      <div className="flex items-center">
         <img className={classesObjectStyles.cardIcon} src={cardIcon} alt="icon" />
         <span className={classesObjectStyles.cardTitle}> {title} </span>
       </div>
